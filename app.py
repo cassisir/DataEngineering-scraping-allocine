3b2ad281860d9e3f7cb90768cfd8128ea73fc6a5
cassisir <ryan.cassisi@edu.esiee.fr>
2024-01-29 15:53:57 +0100

    Webapp Flask : affichage de tous les films dans des cards

--- app.py
+++ app.py
@@ -10,9 +10,23 @@ collection = db.movie_collection
 
 @app.route('/')
 def index():
-    # Retrieve all movies from the MongoDB collection
     movies = list(collection.find())
     return render_template('index.html', movies=movies)
+
+@app.route('/movie/<title>')
+def movie(title):
+    movie = collection.find_one({'title': title})
+    return render_template('movie.html', movie=movie)
+
+# @app.route('/')
+# def accueil():
+#     return "Presentation"
+# @app.route('/film/<string:movie_name>')
+# def presentation_film(movie_name: str):
+#     # Retrieve all movies from the MongoDB collection
+#     return f"Titre : {movie_name}"
+#     # movies = list(collection.find())
+#     # return render_template('index.html', movies=movies)
 
 if __name__ == '__main__':
     app.run(debug=True)
