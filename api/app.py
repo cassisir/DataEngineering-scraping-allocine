8201bd5f25a6428610750c52ed04ac4ab94bf4d0
cassisir <ryan.cassisi@edu.esiee.fr>
2024-02-07 20:22:42 +0100

    reorganisation des fichiers

---
+++ api/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from search_and_filter import apply_filters
+from bson import ObjectId
+from graphs import movies_per_genre_graph, mean_ratings_per_genre_graph, mean_ratings_date_graph
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.allocine
+collection = db.movie_collection
+
+@app.route('/')
+def index():
+    genre_request = request.args.get('genre')
+    sort_by_request = request.args.get('sort_by', 'date')
+    sort_order_request = request.args.get('sort_order', 'descending')
+    search_query = request.args.get('search_query', '').strip()
+    exclude_not_rated = request.args.get('exclude_not_rated') == 'on'
+
+    movies = apply_filters(collection, genre_request, sort_by_request, sort_order_request, exclude_not_rated, search_query)
+
+    all_genres = list(collection.distinct('genre'))
+    return render_template('index.html', movies=movies, all_genres=all_genres, selected_genre=genre_request, selected_sort_by=sort_by_request, selected_order=sort_order_request, exclude_not_rated=exclude_not_rated)
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+    movie = collection.find_one({'_id': movie_id})
+    return render_template('movie.html', movie=movie)
+
+@app.route('/graphs')
+def graphs():
+    movies_per_genre_fig = movies_per_genre_graph(collection)
+    mean_ratings_per_genre = mean_ratings_per_genre_graph(collection)
+    mean_ratings_date = mean_ratings_date_graph(collection)
+
+    return render_template('graphs.html',
+                           movies_per_genre_fig=movies_per_genre_fig,
+                           mean_ratings_per_genre_fig=mean_ratings_per_genre,
+                           mean_ratings_date_fig=mean_ratings_date)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ api/search_and_filter.py
@@ -0,0 +1,54 @@
+from elasticsearch import Elasticsearch
+
+es_client = Elasticsearch()
+index_name = "movies_index"
+doc_type = "movie"
+
+
+def apply_filters(collection, genre_request, sort_by_request, sort_order_request, exclude_not_rated, search_query):
+    query = genre_filter(genre_request)
+    sort = sort_filter(sort_by_request, sort_order_request)
+    researched_movies = search_movies(search_query) # Recherche elasticsearch (retourne les ids des films)
+    
+    if exclude_not_rated:
+        query[sort.get('field')] = {'$ne': 'not rated'}
+    
+    if search_query:
+        query['_id'] = {'$in': researched_movies} # On récupère les informations des ids correspondant dans la base de données Mongo
+    
+    movies = list(collection.find(query).sort(sort.get('field'), sort.get('order')))
+    return movies
+
+def genre_filter(selected_genre):
+    genre_filter = {'genre': selected_genre} if selected_genre else {}
+    return genre_filter
+
+def sort_filter(sort_by, sort_order):
+    # Champs selon lequel trier
+    if sort_by == 'press_rating':
+        sort_field = 'ratings.press'
+    elif sort_by == 'spectators_rating':
+        sort_field = 'ratings.spectators'
+    else:
+        sort_field = 'date'
+
+    # Sens du tri
+    order = 1 if sort_order == 'ascending' else -1
+
+    return {'field':sort_field, 'order':order}
+
+# Recherche texte avec elasticsearch
+def search_movies(search_query):
+    query = {
+        "query": {
+            "multi_match": {
+                "query": search_query,
+                "fields": ["title", "synopsis", "cast.name"],
+                "type": "phrase_prefix",
+            }
+        }
+    }
+    search_results = es_client.search(index=index_name, doc_type=doc_type, body=query, size=2000)
+    movies_results = search_results['hits']['hits']
+    movies_ids = [movie['_id'] for movie in movies_results] # On récupère les ids des films correspondant à la recherche
+    return movies_ids
